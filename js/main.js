fb4a6d5ce9e74301f76a0e5cf9aa436015457c73
acoudray1 <axel.hpm.coudray@gmail.com>
2020-10-09 21:32:18 +0200

    modal box

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function expandText() {
+    document.getElementById("texttoexpand").style.display = "block";
+    document.getElementById("expand").style.display = "none";
+}
+
+function displayModal() {
+    console.log('test');
+    console.log('modal');
+    document.getElementById("modal").style.display = "block";
+}
