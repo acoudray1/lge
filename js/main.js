d8e1e68e1fa69fc1b12c4b913793c403dda61b1d
acoudray1 <axel.hpm.coudray@gmail.com>
2020-10-12 17:07:00 +0200

    wip

--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
-window.onscroll = function() {
-    /*var nav = document.getElementById('nav');
-    if ( window.pageYOffset > 100 ) {
-        nav.classList.add("navbar1");
-    } else {
-        nav.classList.remove("navbar1");
-    }*/
-    document.getElementById("topbar").style.background = black;
-}
-
-window.onload(function(load) {
-    window.addEventListener('scroll', function(evt) {
-        console.log('test');
-    });
-});
-
-console.log('ok');
-
 function expandText() {
     document.getElementById("texttoexpand").style.display = "block";
     document.getElementById("expand").style.display = "none";
