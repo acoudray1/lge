01eaa99423023df007e626d98eae531f65c3a760
acoudray1 <axel.hpm.coudray@gmail.com>
2020-10-12 17:30:46 +0200

    implemented accessibility landmarks

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+window.addEventListener('scroll', () => {
+    console.log('test');
+});
+
 function expandText() {
     document.getElementById("texttoexpand").style.display = "block";
     document.getElementById("expand").style.display = "none";
