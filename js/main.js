a016af8a28e5b3b7bea19dca0f7746ba21691603
acoudray1 <axel.hpm.coudray@gmail.com>
2020-10-12 20:31:56 +0200

    topbar back color wip

--- js/main.js
+++ js/main.js
@@ -2,14 +2,12 @@ function offset(el) {
     var rect = el.getBoundingClientRect(),
     scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
     scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    return { top: rect.top + scrollTop, left: rect.left + scrollLeft, bottom: rect.top + el.offsetHeight }
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft, bottom: scrollTop + el.offsetHeight }
 }
 
 window.onscroll = function() {
     let topbarOffset = offset(document.getElementById("topbar"));
     let bottombarOffset = offset(document.getElementById("bottombar"));
-    console.log(topbarOffset);
-    console.log(bottombarOffset);
     if (topbarOffset.bottom < bottombarOffset.top) {
         document.getElementById("topbar").style.background = "transparent";
     } else if (topbarOffset.bottom >= bottombarOffset.top) {
