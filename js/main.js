f81de4de9a4d5c06feee25fbe38cb46a764dcc02
acoudray1 <axel.hpm.coudray@gmail.com>
2020-10-12 16:04:42 +0200

    added svg and others

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+window.onscroll = function() {
+    /*var nav = document.getElementById('nav');
+    if ( window.pageYOffset > 100 ) {
+        nav.classList.add("navbar1");
+    } else {
+        nav.classList.remove("navbar1");
+    }*/
+    console.log('scrolling');
+}
+
 function expandText() {
     document.getElementById("texttoexpand").style.display = "block";
     document.getElementById("expand").style.display = "none";
