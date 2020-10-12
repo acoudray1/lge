df567a71812850b59aa6164d350edf966dc323f5
acoudray1 <axel.hpm.coudray@gmail.com>
2020-10-12 20:36:16 +0200

    min max func and comments

--- js/main.js
+++ js/main.js
@@ -1,8 +1,33 @@
+/**
+ * Calcul l'offset entre un élément et les rebords de l'écran
+ * 
+ * @param {*} el 
+ */
 function offset(el) {
     var rect = el.getBoundingClientRect(),
     scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
     scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     return { top: rect.top + scrollTop, left: rect.left + scrollLeft, bottom: scrollTop + el.offsetHeight }
+}
+
+/**
+ * Limit a value between a min and max range
+ * 
+ * @param {*} val 
+ * @param {*} min 
+ * @param {*} max 
+ */
+function valueLimit(val, min, max) {
+    return val < min ? min : (val > max ? max : val);
+}
+
+/**
+ * Expands the text on the Future, Love & Death page
+ * 
+ */
+function expandText() {
+    document.getElementById("texttoexpand").style.display = "block";
+    document.getElementById("expand").style.display = "none";
 }
 
 window.onscroll = function() {
@@ -13,9 +38,4 @@ window.onscroll = function() {
     } else if (topbarOffset.bottom >= bottombarOffset.top) {
         document.getElementById("topbar").style.background = "black";
     }
- }
-
-function expandText() {
-    document.getElementById("texttoexpand").style.display = "block";
-    document.getElementById("expand").style.display = "none";
 }
