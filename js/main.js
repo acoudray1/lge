5ed871775128821f7fbf245eb59124d598e4b6cf
acoudray1 <axel.hpm.coudray@gmail.com>
2020-10-12 15:29:16 +0200

    important modification after v2

--- js/main.js
+++ js/main.js
@@ -2,9 +2,3 @@ function expandText() {
     document.getElementById("texttoexpand").style.display = "block";
     document.getElementById("expand").style.display = "none";
 }
-
-function displayModal() {
-    console.log('test');
-    console.log('modal');
-    document.getElementById("modal").style.display = "block";
-}
